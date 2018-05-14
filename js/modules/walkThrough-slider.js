0961c9cf6df04f96572b3b082ca367131dd6fc95
Magdalena Martínez <magdalenamtzg@gmail.com>
2018-05-14 13:39:10 -0500

    Slider added and working. Styling for nav and arrows missing.

---
+++ js/modules/walkThrough-slider.js
@@ -0,0 +1,13 @@
+$(document).ready(function () {
+  jQuery(document).ready(function($) {
+    $('.walkThrough-slider').unslider({
+      autoplay: true,
+      infinite: true,
+      nav: true,
+      arrows: {
+        prev: '<a class="unslider-arrow prev"><img src="/img/arrow-left.svg" alt="" /></a>',
+        next: '<a class="unslider-arrow next"><img src="/img/arrow-right.svg" alt="" /></a>'
+      }
+    });
+  });
+});
